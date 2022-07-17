dc8c3b9d90d2e2cd473f625ef34113cf0328a2fb
mohdnuman <mohammadnuman123@gmail.com>
2022-07-17 14:03:18 +0530

    completed the twitter bot

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ const provider = new ethers.providers.JsonRpcProvider(rpcURL);
 const Twitter = require("twitter");
 
 const client = new Twitter({
-  consumer_key: "7nx3DkuKdwnvmyUJcAxBhgClx",
-  consumer_secret: "4ouCKdFrTtWlTQTVwqswLCyJqXB6GJER7hyduWohJqmZW1lQD6",
-  access_token_key: "1486096407299694593-J9FeKnXEour4QztNcmkMRaToe33dwh",
-  access_token_secret: "f03xBTiN2pA0FoQu0TxgrpCpwEEDPvSvJgPoiMVEl8LpW",
+  consumer_key: "nZ2svFKO1dDiogksJNhH05u76",
+  consumer_secret: "20h8F6WayuYcpEdVgaGo8AN3DflQV2RE6p3OuFIqGlMEQ2D5CH",
+  access_token_key: "1486096407299694593-xXHWqu7N6TwABsNyh17Xbw3Mwnjmyv",
+  access_token_secret: "3Ep5A6hUOTZyy77thXudXKO0r5UzT4hE41imlxlQQnFqZ",
 });
+
 
 const contractAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
 const abi =require("./abi.json");
@@ -20,6 +21,7 @@ const TRANSFER_THRESHOLD = 100000000000;
 
 const main = async () => {
   const name = await contract.name();
+  const symbol=await contract.symbol();
   console.log(
     `Whale tracker statrted! \nListening for large transfers on ${name}`
   );
@@ -29,7 +31,7 @@ const main = async () => {
       client.post(
         "statuses/update",
         {
-          status: `New whale transfer for ${name}: https://etherscan.io/tx/${data.transactionHash} from ${from} to ${to}`,
+          status: `New whale transfer for ${name}: https://etherscan.io/tx/${data.transactionHash} ${(amount/10**6).toFixed(2)} ${symbol} transferred from ${from} to ${to}`,
         },
         function (error, tweet, response) {
           if (error) throw error;
